2a4bb146de50c9ba1556a1afb6711968bf6f3125
Christopher Antila <christopher@antila.ca>
2015-12-21 14:45:43 -0500

    version ==> 0.2.2

--- source/conf.py
+++ source/conf.py
@@ -52,7 +52,7 @@ copyright = u'2015, Christopher Antila'
 # built documents.
 #
 # The short X.Y version.
-version = '0.2.1'
+version = '0.2.2'
 # The full version, including alpha/beta/rc tags.
 release = '{}'.format(version)
 
